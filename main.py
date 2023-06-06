88f57c2955af4a1663c3d880eb89a04b415bc86a
goldman2g5 <95956749+goldman2g5@users.noreply.github.com>
2023-06-06 18:46:54 +0300

    пишет в бд

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import requests
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
@@ -19,6 +20,7 @@ dp = Dispatcher(bot, storage=storage)
 class AddChannelStates(StatesGroup):
     waiting_for_channel_name = State()
     waiting_for_check = State()
+    waiting_for_channel_description = State()
 
 
 # Функция для проверки добавления бота на канал
@@ -50,8 +52,8 @@ async def process_menu_callbacks(callback_query: types.CallbackQuery):
     if callback_query.data == "add_channel":
         await AddChannelStates.waiting_for_channel_name.set()
         await callback_query.message.answer("Для размещения канала на мониторинге, "
-                                             "нужно добавить бота на канал. "
-                                             "Пожалуйста, введите имя канала:")
+                                            "нужно добавить бота на канал. "
+                                            "Пожалуйста, введите имя канала:")
     elif callback_query.data == "manage_channels":
         await callback_query.message.answer("Функционал управления каналами в разработке.")
 
@@ -72,6 +74,7 @@ async def process_channel_name(message: types.Message, state: FSMContext):
 
     await AddChannelStates.waiting_for_check.set()
 
+
 # Обработчик нажатия на кнопку "Добавить бота"
 @dp.callback_query_handler(state=AddChannelStates.waiting_for_check, text="add_bot")
 async def process_add_bot(callback_query: types.CallbackQuery, state: FSMContext):
@@ -82,25 +85,64 @@ async def process_add_bot(callback_query: types.CallbackQuery, state: FSMContext
 
     # Проверяем добавление бота на канал
     if await check_bot_in_channel(channel_name):
-        await state.finish()
-        await callback_query.message.answer("Успешно! Бот добавлен на канал")
+        # Проверяем, является ли пользователь администратором канала
+        try:
+            chat = await bot.get_chat(channel_name)
+            member = await bot.get_chat_member(chat.id, user.id)
+            if member.status in ("administrator", "creator"):
+                # Store channel_name in the state to access it in the next handler
+                await state.update_data(channel_name=channel_name)
+                await callback_query.message.answer("Успешно! Бот добавлен на канал.")
+                await callback_query.message.answer("Please enter the channel description:")
+                await AddChannelStates.waiting_for_channel_description.set()
+                return
+            else:
+                message = "Ошибка: вы не являетесь администратором указанного канала."
+        except Exception as e:
+            message = f"Произошла ошибка при проверке: {e}"
     else:
-        await callback_query.message.answer("Ошибка: бот не был добавлен на канал.")
+        message = "Ошибка: бот не был добавлен на канал."
 
-    # Проверяем, является ли пользователь администратором канала
-    try:
-        chat = await bot.get_chat(channel_name)
-        member = await bot.get_chat_member(chat.id, user.id)
-        if member.status in ("administrator", "creator"):
-            await state.finish()
-            await callback_query.message.answer("Успешно! Пользователь является администратором канала.")
-        else:
-            await callback_query.message.answer("Ошибка: вы не являетесь администратором указанного канала.")
-    except Exception as e:
-        await callback_query.message.answer(f"Произошла ошибка при проверке: {e}")
+    await callback_query.message.answer(message)
 
 
+# Обработчик ввода описания
+@dp.message_handler(state=AddChannelStates.waiting_for_channel_description)
+async def process_channel_description(message: types.Message, state: FSMContext):
+    data = await state.get_data()
+    channel_name = data.get("channel_name")
 
+    print(channel_name)
+    if channel_name is None:
+        # Handle the case when channel_name is not available in the state
+        await message.answer("Ошибка: Не удалось получить информацию о канале.")
+
+        return
+
+    channel_description = message.text
+
+    # Update the channel description in the state
+    await state.update_data(channel_description=channel_description)
+
+    # Retrieve the member count
+    chat = await bot.get_chat(channel_name)
+    members_count = await bot.get_chat_members_count(chat.id)
+
+    # Write channel information to the database using the API
+    api_url = "http://localhost:8053/api/Channel"
+
+    data = {
+        "Name": channel_name,
+        "Description": channel_description,
+        "Members": members_count
+    }
+
+    response = requests.post(api_url, json=data)
+    if response.status_code == 201:
+        await message.answer("Channel information saved successfully.")
+    else:
+        await message.answer("Failed to save channel information.")
+    await state.finish()
 
 if __name__ == "__main__":
     # Запуск бота
