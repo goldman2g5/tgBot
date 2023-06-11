76b551fac5787863664e367dd9848395062f0bf3
goldman2g5 <95956749+goldman2g5@users.noreply.github.com>
2023-06-11 08:24:21 +0300

    еще одна менюшка, ща крадик мутить буду

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import base64
 import logging
+from typing import List
+
 import requests
 import io
 import json
@@ -8,7 +10,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Command
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, User
 from aiogram.utils import exceptions
 
 # Set the log level for debugging
@@ -25,6 +27,7 @@ class AddChannelStates(StatesGroup):
     waiting_for_channel_name = State()
     waiting_for_check = State()
     waiting_for_channel_description = State()
+    waiting_for_user_id = State()  # New state for user ID
 
 
 # Function to check if the bot is added to the channel
@@ -54,6 +57,16 @@ async def cmd_start(message: types.Message):
     await open_menu(message.chat.id)
 
 
+# Function to retrieve user's channels from the API
+def get_user_channels(user_id: int) -> List[dict]:
+    api_url = f"http://localhost:8053/api/Channel/ByUser/{user_id}"
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return []
+
+
 # Handler for menu button callbacks
 @dp.callback_query_handler(lambda c: c.data in ["add_channel", "manage_channels"])
 async def process_menu_callbacks(callback_query: types.CallbackQuery):
@@ -61,7 +74,45 @@ async def process_menu_callbacks(callback_query: types.CallbackQuery):
         await AddChannelStates.waiting_for_channel_name.set()
         await callback_query.message.answer("Please enter the channel name:")
     elif callback_query.data == "manage_channels":
-        await callback_query.message.answer("Channel management functionality is under development.")
+        # Get the user's ID
+        user_id = callback_query.from_user.id
+
+        # Retrieve the user's channels from the API
+        channels = get_user_channels(user_id)
+
+        if channels:
+            # Create inline buttons for each channel
+            markup = InlineKeyboardMarkup(row_width=1)
+            for channel in channels:
+                button_text = f"{channel['name']} - {channel['description']}"
+                callback_data = f"channel_{channel['id']}"
+                markup.add(InlineKeyboardButton(button_text, callback_data=callback_data))
+            await callback_query.message.answer("Your channels:", reply_markup=markup)
+        else:
+            await callback_query.message.answer("You don't have any channels.")
+
+
+# Handler for channel menu callbacks
+@dp.callback_query_handler(lambda c: c.data.startswith("channel_") or c.data == "back_to_menu")
+async def process_channel_menu(callback_query: types.CallbackQuery):
+    if callback_query.data == "back_to_menu":
+        # Open the main menu
+        await open_menu(callback_query.message.chat.id)
+        return
+
+    channel_id = int(callback_query.data.split("_")[1])
+
+    # Create inline buttons for channel menu
+    markup = InlineKeyboardMarkup(row_width=1)
+    markup.add(
+        InlineKeyboardButton("Subscription", callback_data=f"subscription_{channel_id}"),
+        InlineKeyboardButton("Notifications", callback_data=f"notifications_{channel_id}"),
+        InlineKeyboardButton("Bump", callback_data=f"bump_{channel_id}"),
+        InlineKeyboardButton("Customization", callback_data=f"customization_{channel_id}"),
+        InlineKeyboardButton("Back to Menu", callback_data="back_to_menu")
+    )
+
+    await callback_query.message.answer("Channel menu:", reply_markup=markup)
 
 
 # Handler for entering the channel name
@@ -96,8 +147,8 @@ async def process_add_bot(callback_query: types.CallbackQuery, state: FSMContext
             chat = await bot.get_chat(channel_name)
             member = await bot.get_chat_member(chat.id, user.id)
             if member.status in ("administrator", "creator"):
-                # Store channel_name in the state to access it in the next handler
-                await state.update_data(channel_name=channel_name)
+                # Store channel_name and user_id in the state
+                await state.update_data(channel_name=channel_name, user_id=user.id)
                 await callback_query.message.answer("Success! Bot added to the channel.")
                 await callback_query.message.answer("Please enter the channel description:")
                 await AddChannelStates.waiting_for_channel_description.set()
@@ -117,6 +168,7 @@ async def process_add_bot(callback_query: types.CallbackQuery, state: FSMContext
 async def process_channel_description(message: types.Message, state: FSMContext):
     data = await state.get_data()
     channel_name = data.get("channel_name")
+    user_id = data.get("user_id")  # Retrieve the user's ID from the state
 
     if channel_name is None:
         # Handle the case when channel_name is not available in the state
@@ -127,7 +179,7 @@ async def process_channel_description(message: types.Message, state: FSMContext)
     channel_description = message.text
 
     # Save channel information to the database
-    if await save_channel_information(channel_name, channel_description):
+    if await save_channel_information(channel_name, channel_description, user_id):  # Pass user_id to the function
         await message.answer("Channel information saved successfully.")
     else:
         await message.answer("Failed to save channel information.")
@@ -138,7 +190,7 @@ async def process_channel_description(message: types.Message, state: FSMContext)
 
 
 # Function to save channel information to the database
-async def save_channel_information(channel_name: str, channel_description: str) -> bool:
+async def save_channel_information(channel_name: str, channel_description: str, user_id: int) -> bool:
     # Retrieve the member count
     chat = await bot.get_chat(channel_name)
     members_count = await bot.get_chat_members_count(chat.id)
@@ -156,17 +208,16 @@ async def save_channel_information(channel_name: str, channel_description: str) 
 
     # Write channel information to the database using the API
     api_url = "http://localhost:8053/api/Channel"
+    print(user_id)
 
     data = {
         "id": 0,
         "name": channel_name,
         "description": channel_description,
         "members": members_count,
-        "avatar": avatar_base64
+        "avatar": avatar_base64,
+        "user": user_id  # Add the user ID to the data payload
     }
-
-    json_data = json.dumps(data)  # Serialize the data to JSON
-    print(json_data)
 
     response = requests.post(api_url, json=data)
     print(response.text)
