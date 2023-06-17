801bda270a0620a6c3d10a898a1303f78e49b4e2
goldman2g5 <95956749+goldman2g5@users.noreply.github.com>
2023-06-17 19:02:13 +0300

    уведомления

--- main.py
+++ main.py
@@ -116,6 +116,121 @@ async def process_channel_menu(callback_query: types.CallbackQuery):
     await callback_query.message.answer("Channel menu:", reply_markup=markup)
 
 
+# Handler for notifications button
+@dp.callback_query_handler(lambda c: c.data.startswith("notifications_"))
+async def process_notifications_button(callback_query: types.CallbackQuery):
+    channel_id = int(callback_query.data.split("_")[1])
+
+    # Retrieve the current notification status from the API
+    api_url = f"http://localhost:8053/api/Channel/{channel_id}"
+    response = requests.get(api_url)
+    if response.status_code == 404:
+        # Channel not found
+        await callback_query.message.answer("Channel not found.")
+        return
+
+    channel_data = response.json()
+    notifications_enabled = channel_data.get("notifications")
+
+    if notifications_enabled is None:
+        notifications_enabled = False  # Assume notifications are disabled if the value is null
+
+    # Create inline buttons for notifications menu
+    markup = InlineKeyboardMarkup(row_width=1)
+    toggle_text = "Disable" if notifications_enabled else "Enable"
+    toggle_callback_data = f"toggle_notifications_{channel_id}"
+    markup.add(
+        InlineKeyboardButton(f"{toggle_text} Notifications", callback_data=toggle_callback_data),
+        InlineKeyboardButton("Back to Menu", callback_data="back_to_menu")
+    )
+
+    # Edit the existing message with the updated notification status and toggle button
+    await bot.edit_message_text(
+        chat_id=callback_query.message.chat.id,
+        message_id=callback_query.message.message_id,
+        text=f"Notifications is {'on' if notifications_enabled else 'off'}",
+        reply_markup=markup
+    )
+
+# Handler for toggle/disable button
+@dp.callback_query_handler(lambda c: c.data.startswith("toggle_notifications_"))
+async def process_toggle_notifications_button(callback_query: types.CallbackQuery):
+    channel_id = int(callback_query.data.split("_")[2])
+
+    # Retrieve the current notification status from the API
+    api_url = f"http://localhost:8053/api/Channel/{channel_id}"
+    response = requests.get(api_url)
+    if response.status_code == 404:
+        # Channel not found
+        await callback_query.message.answer("Channel not found.")
+        return
+
+    channel_data = response.json()
+    notifications_enabled = channel_data.get("notifications")
+
+    if notifications_enabled is None:
+        notifications_enabled = False  # Assume notifications are disabled if the value is null
+
+    # Toggle the notification status
+    new_notifications_enabled = not notifications_enabled
+
+    # Update the notification status in the API using a PUT request
+    api_toggle_url = f"http://localhost:8053/api/Channel/ToggleNotifications/{channel_id}"
+    put_data = {"notifications": new_notifications_enabled}
+    response = requests.put(api_toggle_url, json=put_data)
+    if response.status_code != 204:
+        await callback_query.message.answer("Failed to toggle notifications.")
+        return
+
+    # Create inline buttons for notifications menu
+    markup = InlineKeyboardMarkup(row_width=1)
+    toggle_text = "Disable" if new_notifications_enabled else "Enable"
+    toggle_callback_data = f"toggle_notifications_{channel_id}"
+    markup.add(
+        InlineKeyboardButton(f"{toggle_text} Notifications", callback_data=toggle_callback_data),
+        InlineKeyboardButton("Back to Menu", callback_data="back_to_menu")
+    )
+
+    # Edit the existing message with the updated notification status and toggle button
+    await bot.edit_message_text(
+        chat_id=callback_query.message.chat.id,
+        message_id=callback_query.message.message_id,
+        text=f"Notifications is {'on' if new_notifications_enabled else 'off'}",
+        reply_markup=markup
+    )
+
+# Handler for subscription button
+@dp.callback_query_handler(lambda c: c.data.startswith("subscription_"))
+async def process_subscription_button(callback_query: types.CallbackQuery):
+    channel_id = int(callback_query.data.split("_")[1])
+
+    # Create inline buttons for subscription options
+    markup = InlineKeyboardMarkup(row_width=1)
+    markup.add(
+        InlineKeyboardButton("Lite", callback_data=f"subscription_choice_{channel_id}_lite"),
+        InlineKeyboardButton("Pro", callback_data=f"subscription_choice_{channel_id}_pro"),
+        InlineKeyboardButton("Premium", callback_data=f"subscription_choice_{channel_id}_premium"),
+        InlineKeyboardButton("Back to Menu", callback_data="back_to_menu")
+    )
+
+    with open('subscription_image.jpg', 'rb') as photo_file:
+        await callback_query.message.reply_photo(photo_file, reply_markup=markup)
+
+
+# Handler for subscription choice buttons
+@dp.callback_query_handler(lambda c: c.data.startswith("subscription_choice_"))
+async def process_subscription_choice(callback_query: types.CallbackQuery):
+    choice_data = callback_query.data.split("_")
+    channel_id = int(choice_data[2])
+    subscription_type = choice_data[3]
+
+    # Process the subscription choice
+    # ...
+
+    # Send a message indicating the chosen subscription
+    await callback_query.message.answer(f"You have chosen {subscription_type} subscription for channel {channel_id}.")
+
+
 # Handler for entering the channel name
 @dp.message_handler(state=AddChannelStates.waiting_for_channel_name)
 async def process_channel_name(message: types.Message, state: FSMContext):
@@ -217,7 +332,7 @@ async def save_channel_information(channel_name: str, channel_description: str, 
         "description": channel_description,
         "members": members_count,
         "avatar": avatar_base64,
-        "user": user_id  # Add the user ID to the data payload
+        "user": user_id
     }
 
     response = requests.post(api_url, json=data)
