19f746dcb585f9e38204cad3ede57a729784b862
goldman2g5 <95956749+goldman2g5@users.noreply.github.com>
2023-06-25 19:30:40 +0300

    залил говно сиропом

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ from aiogram.contrib.fsm_storage.redis import RedisStorage2
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher.middlewares import BaseMiddleware
-
 import asyncio
 import time
 import requests
@@ -23,76 +22,62 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.middlewares import BaseMiddleware
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+
+# Initialize APScheduler scheduler
+scheduler = AsyncIOScheduler()
+
+
+async def send_notification(notification):
+    channel_name = notification['channelName']
+    send_time = notification['sendTime']
+    telegram_user_id = notification['telegramUserId']
+    telegram_chat_id = notification['telegramChatId']
+    channel_id = notification['channelId']
+
+    # Send the notification to the user using the bot
+    markup = types.InlineKeyboardMarkup(row_width=1)
+    markup.add(
+        types.InlineKeyboardButton("Bump", callback_data=f"bump_{channel_id}"),
+    )
+    await bot.send_message(telegram_chat_id, f"It's time to bump {channel_name}!", reply_markup=markup)
+
+
+async def fetch_notifications():
+    try:
+        # Fetch notifications from the API
+        response = requests.get('http://localhost:8053/api/Notification')
+        print("OCHELLO")
+        response.raise_for_status()  # Raise an exception for non-2xx responses
+
+        notifications = response.json()
+
+        # Process each notification
+        for notification in notifications:
+            await send_notification(notification)
+
+    except requests.RequestException as e:
+        # Log the error
+        print(f"Error fetching notifications: {str(e)}")
+
 
 async def check_notifications():
-    while True:
-        try:
-            # Fetch notifications from the API
-            print("OCHELLO POLNOE")
-            response = requests.get('http://localhost:8053/api/Notification')
-            print(f"{response.json()}")
-            response.raise_for_status()  # Raise an exception for non-2xx responses
+    # Fetch notifications from the API
+    await fetch_notifications()
 
-            notifications = response.json()
 
-            # Process each notification
-            for notification in notifications:
-                # Extract required information from the notification
-                channel_name = notification['channelName']
-                send_time = notification['sendTime']
-                telegram_user_id = notification['telegramUserId']
-                telegram_chat_id = notification['telegramChatId']
-                channel_id = notification['channelId']
+async def on_bot_start_up(dispatcher: Dispatcher):
+    # Start the APScheduler scheduler
+    scheduler.add_job(check_notifications, IntervalTrigger(minutes=1))
+    scheduler.start()
 
-                # Send the notification to the user using the bot
-                await asyncio.sleep(5)
 
-                markup = InlineKeyboardMarkup(row_width=1)
-                markup.add(
-                    InlineKeyboardButton("Bump", callback_data=f"bump_{channel_id}"),
-                )
-                await bot.send_message(telegram_chat_id, f"Its time to bump {channel_name}!", reply_markup=markup)
-
-        except (requests.RequestException, json.JSONDecodeError) as e:
-            # Log the error and continue the loop
-            print(f"Error fetching notifications: {str(e)}")
-
-        # Wait for 30 minutes before checking for new notifications again
-        await asyncio.sleep(1 * 60)
-
-async def background_on_start() -> None:
-    """Background task which is created when bot starts"""
-    while True:
-        await asyncio.sleep(5)
-        print("Hello World!")
-
-async def background_on_action() -> None:
-    """Background task which is created when the user asked"""
-    for _ in range(20):
-        await asyncio.sleep(3)
-        print("Action!")
-
-async def background_task_creator(message: types.Message) -> None:
-    """Creates background tasks"""
-    asyncio.create_task(background_on_action())
-    await message.reply("Another one background task create")
-
-async def on_bot_start_up(dispatcher: Dispatcher) -> None:
-    """List of actions which should be done before bot start"""
-    asyncio.create_task(check_notifications())  # Creates background task
-
-def create_bot_factory() -> None:
-    """Creates and starts the bot"""
-    # Create a Redis client
-    redis_client = redis.Redis(host='localhost', port=6379, db=0)
-
-    # Create an instance of the bot and the dispatcher
-
-    # Bot endpoints block:
-    dp.register_message_handler(background_task_creator)
-
-    # Start the bot
+def create_bot_factory():
+    # Start the bot and register the startup function
     executor.start_polling(dp, skip_updates=True, on_startup=on_bot_start_up)
+
 
 # Start the bot
 if __name__ == '__main__':
