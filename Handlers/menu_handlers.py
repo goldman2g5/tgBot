539093b46d863d9f92a5261573a2e1b2810eabb1
goldman2g5 <95956749+goldman2g5@users.noreply.github.com>
2023-06-25 20:09:30 +0300

    ломаю бота

--- Handlers/menu_handlers.py
+++ Handlers/menu_handlers.py
@@ -17,27 +17,27 @@ async def cmd_start(message: types.Message):
     await open_menu(message.chat.id)
 
 
-# Handler for menu button callbacks
-@dp.callback_query_handler(lambda c: c.data in ["add_channel", "manage_channels"])
-async def process_menu_callbacks(callback_query: types.CallbackQuery):
-    if callback_query.data == "add_channel":
-        await AddChannelStates.waiting_for_channel_name.set()
-        await callback_query.message.answer("Please enter the channel name:")
+@dp.callback_query_handler(lambda c: c.data == "add_channel")
+async def add_channel_handler(callback_query: types.CallbackQuery):
+    await AddChannelStates.waiting_for_channel_name.set()
+    await callback_query.message.answer("Please enter the channel name:")
 
-    elif callback_query.data == "manage_channels":
-        # Get the user's ID
-        user_id = callback_query.from_user.id
 
-        # Retrieve the user's channels from the API
-        channels = get_user_channels(user_id)
+@dp.callback_query_handler(lambda c: c.data == "manage_channels")
+async def manage_channels_handler(callback_query: types.CallbackQuery):
+    # Get the user's ID
+    user_id = callback_query.from_user.id
 
-        if channels:
-            # Create inline buttons for each channel
-            markup = InlineKeyboardMarkup(row_width=1)
-            for channel in channels:
-                button_text = f"{channel['name']} - {channel['description']}"
-                callback_data = f"channel_{channel['id']}_{channel['name']}"
-                markup.add(InlineKeyboardButton(button_text, callback_data=callback_data))
-            await callback_query.message.answer("Your channels:", reply_markup=markup)
-        else:
-            await callback_query.message.answer("You don't have any channels.")
+    # Retrieve the user's channels from the API
+    channels = get_user_channels(user_id)
+
+    if channels:
+        # Create inline buttons for each channel
+        markup = InlineKeyboardMarkup(row_width=1)
+        for channel in channels:
+            button_text = f"{channel['name']} - {channel['description']}"
+            callback_data = f"channel_{channel['id']}"
+            markup.add(InlineKeyboardButton(button_text, callback_data=callback_data))
+        await callback_query.message.answer("Your channels:", reply_markup=markup)
+    else:
+        await callback_query.message.answer("You don't have any channels.")
