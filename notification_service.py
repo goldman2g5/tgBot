fb56cda21bcb307cc8ef4dd80cdfcd654c95f4fe
goldman2g5 <95956749+goldman2g5@users.noreply.github.com>
2023-07-08 08:24:15 +0300

    подписка отписывается

--- notification_service.py
+++ notification_service.py
@@ -9,10 +9,8 @@ from Handlers.add_channel_handlers import *
 scheduler = AsyncIOScheduler()
 
 
-async def send_notification(notification):
+async def send_bump_notification(notification):
     channel_name = notification['channelName']
-    send_time = notification['sendTime']
-    telegram_user_id = notification['telegramUserId']
     telegram_chat_id = notification['telegramChatId']
     channel_id = notification['channelId']
 
@@ -24,18 +22,36 @@ async def send_notification(notification):
     await bot.send_message(telegram_chat_id, f"It's time to bump {channel_name}!", reply_markup=markup)
 
 
+async def send_sub_notification(notification):
+    channel_name = notification['channelName']
+    telegram_chat_id = notification['telegramChatId']
+    channel_id = notification['channelId']
+
+    await bot.send_message(telegram_chat_id, f"Your subsciption for {channel_name} has expired!")
+
+
 async def fetch_notifications():
     try:
         # Fetch notifications from the API
         response = requests.get('http://localhost:8053/api/Notification')
-        print("OCHELLO")
         response.raise_for_status()  # Raise an exception for non-2xx responses
 
-        notifications = response.json()
+        bump_notifications = response.json()
 
         # Process each notification
-        for notification in notifications:
-            await send_notification(notification)
+        for notification in bump_notifications:
+            await send_bump_notification(notification)
+
+        response = requests.get('http://localhost:8053/api/Subscription/CheckExpiredSubscriptions')
+        response.raise_for_status()  # Raise an exception for non-2xx responses
+
+        sub_notifications = response.json()
+
+        # Process each notification
+        for notification in sub_notifications:
+            await send_sub_notification(notification)
+
+
 
     except requests.RequestException as e:
         # Log the error
