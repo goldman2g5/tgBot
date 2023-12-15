8740b73449c194f4c021965f36aeafb6d3567504
goldman2g5 <95956749+goldman2g5@users.noreply.github.com>
2023-12-16 00:03:29 +0300

    сохраняюсь

--- socket_service.py
+++ socket_service.py
@@ -6,6 +6,21 @@ import json
 negotiation = requests.post('http://localhost:7256/BotHub/negotiate?negotiateVersion=0').json()
 
 
+def sum_of_two(number1, number2):
+    print("function call")
+    try:
+        # Ensure that the parameters are numbers
+        num1 = float(number1)
+        num2 = float(number2)
+    except ValueError:
+        # Return an error if the parameters are not numbers
+        return {"error": "Invalid input. Please provide valid numbers."}
+
+    # Calculate the sum
+    total = num1 + num2
+    return {"result": total}
+
+
 def toSignalRMessage(data):
     return f'{json.dumps(data)}\u001e'
 
@@ -28,6 +43,53 @@ async def connectToHub(connectionId):
             while _running:
                 get_response = await websocket.recv()
                 print(f"get_response: {get_response}")
+
+                # Find the end of the JSON object and trim the string
+                end_of_json = get_response.rfind("}") + 1
+                json_string = get_response[:end_of_json]
+
+                # Parse the incoming JSON message
+                try:
+                    outer_message_data = json.loads(json_string)
+                except json.JSONDecodeError as e:
+                    print(f"JSON Decode Error: {e}")
+                    continue  # Skip processing for this message
+
+                # Check for the 'arguments' field which contains the actual function call
+                if "arguments" in outer_message_data and outer_message_data["arguments"]:
+                    try:
+                        # The function call data is expected to be a JSON string inside the 'arguments' array
+                        function_call_data = json.loads(outer_message_data["arguments"][0])
+                    except (IndexError, json.JSONDecodeError) as e:
+                        print(f"Error processing arguments: {e}")
+                        continue  # Skip processing this message
+
+                    function_name = function_call_data.get("functionName")
+                    parameters = function_call_data.get("parameters", {})
+
+                    # Map function names to actual function calls
+                    function_map = {
+                        "sumOfTwo": sum_of_two,  # Adding the sum_of_two function
+                        # Add other functions here as needed
+                    }
+
+                    # Call the appropriate function
+                    if function_name in function_map:
+                        try:
+                            result = function_map[function_name](**parameters)
+                        except TypeError as e:
+                            result = {"error": f"Invalid parameters for {function_name}: {str(e)}"}
+                        except Exception as e:
+                            result = {"error": f"Error in {function_name}: {str(e)}"}
+                    else:
+                        result = {"error": "Unknown function"}
+                else:
+                    print("Not a function call type message. Skipping.")
+                    continue
+
+                # Send the result back
+                print(result)
+                await websocket.send(json.dumps(result))
 
         await handshake()
 
@@ -74,6 +136,5 @@ async def connectToHub(connectionId):
             "invocationId": "stream_id"
         }
         await websocket.send(toSignalRMessage(completion_message))
-        _running = False
         await ping_task
         await listen_task
