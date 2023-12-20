48e237183f01155c8175ae6249ee673bd7d45bde
lawrari <xtcfakelv@gmail.com>
2023-12-21 00:56:14 +0300

    не умирай не умирай не умирай не умирай не умирай

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiotdlib import Client
 from pydantic import SecretStr
+from pyrogram import Client as pyroClient
 from aiotdlib.api import UpdateAuthorizationState, AuthorizationStateWaitPhoneNumber, AuthorizationStateWaitCode
 
 # Set the log level for debugging
@@ -11,18 +12,24 @@ logging.basicConfig(level=logging.INFO)
 API_ID = 23558497
 API_HASH = "2b461873dd2dea7e091f7af28fbe11e1"
 api_hash_secret = SecretStr('API_HASH')
-bot_token = '6073155840:AAEq_nWhpl5qHjIpEEHKQ0cq9GeF_l0cJo4'
-bot_token_secret = SecretStr('6073155840:AAEq_nWhpl5qHjIpEEHKQ0cq9GeF_l0cJo4')
-PHONE_NUMBER = "+79103212166"
+bot_token = '6488513477:AAHYdfO0qq_JhbsjVEYFyPpbtxUaxMkWomc'
+bot_token_secret = SecretStr('6488513477:AAHYdfO0qq_JhbsjVEYFyPpbtxUaxMkWomc')
+PHONE_NUMBER = "+79168355570"
 
 client = Client(
      api_id=API_ID,
      api_hash=API_HASH,
      phone_number=PHONE_NUMBER,
-     library_path='C:/td/tdlib/bin/tdjson.dll'
+     library_path='D:\\Program Files (D)\\td\\tdlib\\bin\\tdjson.dll'
+)
+
+pyro_client = pyroClient(
+    "my_account",
+    api_id=API_ID,
+    api_hash=API_HASH
 )
 
 # Initialize the bot and dispatcher
-bot = Bot(token="6073155840:AAEq_nWhpl5qHjIpEEHKQ0cq9GeF_l0cJo4")
+bot = Bot(token="6488513477:AAHYdfO0qq_JhbsjVEYFyPpbtxUaxMkWomc")
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
--- notification_service.py
+++ notification_service.py
@@ -115,5 +115,6 @@ async def check_notifications():
 
 
 async def start_notification_service(dispatcher: Dispatcher):
+    print("Starting notification service...")
     scheduler.add_job(check_notifications, IntervalTrigger(seconds=500))
     scheduler.start()
