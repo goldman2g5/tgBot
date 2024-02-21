31e843deba03f887383241081178378eb629b9c1
goldman2g5 <95956749+goldman2g5@users.noreply.github.com>
2024-02-21 04:23:26 +0300

    jihad

--- api.py
+++ api.py
@@ -5,8 +5,8 @@ from typing import List, Optional
 import aiohttp
 import requests
 
-# API_URL = "http://localhost:7256/api"
-API_URL = "https://tgsearch.info:8443/api"
+API_URL = "http://localhost:7256/api"
+#API_URL = "https://tgsearch.info:8443/api"
 API_KEY = "7bdf1ca44d84484c9864c06c0aedc1beb740909b02e4404ebafd381db897e1a5387567f8b42f47c7b5192eac60547460e0003c11fd804d1a966a30eacd939a3acaa9a352797f436aad6cd14f27517554"
 Verify_value = False
 
@@ -46,7 +46,7 @@ async def save_channel_information(channel_name: str, channel_description: str, 
         "description": channel_description,
         "members": members_count,
         "avatar": avatar_base64,
-        "user": user_id,
+        "userTelegramID": user_id,
         "telegramId": channel_id,
         "language": language,
         "flag": flag,
